d976ba2beaedb7b740d444f3979c7539aacf9811
itorly <itorly@163.com>
2025-04-10 17:11:13 +0800

    1 Quick start guide 1.1 A simple example

---
+++ quick_start_guide/quick_start_guide.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# 1 Quick start guide
+# 1.1 A simple example
+fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
+plt.show()                           # Show the figure.
+
+
+
