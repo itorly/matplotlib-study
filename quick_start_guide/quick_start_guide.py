ad070779f63d67b9ca383fb5e6563b7319087045
itorly <itorly@163.com>
2025-04-10 17:53:36 +0800

    1.2 Parts of a Figure 1.2.1 The Figure

--- quick_start_guide/quick_start_guide.py
+++ quick_start_guide/quick_start_guide.py
@@ -6,5 +6,13 @@ fig, ax = plt.subplots()             # Create a figure containing a single Axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
 plt.show()                           # Show the figure.
 
+# 1.2 Parts of a Figure
+# 1.2.1 The Figure
+fig = plt.figure()             # an empty figure with no Axes
+fig, ax = plt.subplots()       # a figure with a single Axes
+fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+# a figure with one Axes on the left, and two on the right:
+fig, axs = plt.subplot_mosaic([['left', 'right_top'],
+                               ['left', 'right_bottom']])
 
-
+plt.show()
