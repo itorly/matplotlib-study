0582f95e9226f9cd7a30484870ccb4ecf30d2640
itorly <itorly@163.com>
2025-04-29 23:46:42 +0800

    1.2 Parts of a Figure.png

--- quick_start_guide/quick_start_guide.py
+++ quick_start_guide/quick_start_guide.py
@@ -4,18 +4,20 @@ import numpy as np
 # # 1.1 A simple example
 # fig, ax = plt.subplots()             # Create a figure containing a single Axes.
 # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
-# plt.show()                           # Show the figure.
-#
-# # 1.2 Parts of a Figure
-# # 1.2.1 The Figure
-# fig = plt.figure()             # an empty figure with no Axes
-# fig, ax = plt.subplots()       # a figure with a single Axes
-# fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
-# # a figure with one Axes on the left, and two on the right:
-# fig, axs = plt.subplot_mosaic([['left', 'right_top'],
-#                                ['left', 'right_bottom']])
-#
+# # plt.show()                           # Show the figure.
+# plt.savefig("1.1 A simple example.png")
+
+# 1.2 Parts of a Figure
+# 1.2.1 The Figure
+fig = plt.figure()             # an empty figure with no Axes
+fig, ax = plt.subplots()       # a figure with a single Axes
+fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+# a figure with one Axes on the left, and two on the right:
+fig, axs = plt.subplot_mosaic([['left', 'right_top'],
+                               ['left', 'right_bottom']])
+
 # plt.show()
+plt.savefig("1.2 Parts of a Figure.png")
 
 # 1.3 Types of inputs to plotting functions
 # b = np.matrix([[1, 2], [3, 4]])
