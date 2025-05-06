ca8245c2437341e8164e410aa4a4c8458afb9250
itorly <itorly@163.com>
2025-05-06 23:55:45 +0800

    1.6.3 Annotations

--- quick_start_guide/quick_start_guide.py
+++ quick_start_guide/quick_start_guide.py
@@ -137,22 +137,37 @@ data1, data2, data3, data4 = np.random.randn(4, 100)  # make 4 random data sets
 
 # 1.6.1 Axes labels and text
 
-mu, sigma = 115, 15
-x = mu + sigma * np.random.randn(10000)
-fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-# the histogram of the data
-n, bins, patches = ax.hist(x, 50, density=True, facecolor='C0', alpha=0.75)
-
-ax.set_xlabel('Length [cm]')
-# t = ax.set_xlabel('my data', fontsize=14, color='red')
-ax.set_ylabel('Probability')
-ax.set_title('Aardvark lengths\n (not really)')
-ax.text(75, .025, r'$\mu=115,\ \sigma=15$')
-ax.axis([55, 175, 0, 0.03])
-ax.grid(True)
-# plt.show()
-plt.savefig("1.6.1 Axes labels and text.png")
+# mu, sigma = 115, 15
+# x = mu + sigma * np.random.randn(10000)
+# fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+# # the histogram of the data
+# n, bins, patches = ax.hist(x, 50, density=True, facecolor='C0', alpha=0.75)
+#
+# ax.set_xlabel('Length [cm]')
+# # t = ax.set_xlabel('my data', fontsize=14, color='red')
+# ax.set_ylabel('Probability')
+# ax.set_title('Aardvark lengths\n (not really)')
+# ax.text(75, .025, r'$\mu=115,\ \sigma=15$')
+# ax.axis([55, 175, 0, 0.03])
+# ax.grid(True)
+# # plt.show()
+# plt.savefig("1.6.1 Axes labels and text.png")
 
 # 1.6.2 Using mathematical expressions in text
 
-ax.set_title(r'$\sigma_i=15$')
+# ax.set_title(r'$\sigma_i=15$')
+
+# 1.6.3 Annotations
+
+fig, ax = plt.subplots(figsize=(5, 2.7))
+
+t = np.arange(0.0, 5.0, 0.01)
+s = np.cos(2 * np.pi * t)
+line, = ax.plot(t, s, lw=2)
+
+ax.annotate('local max', xy=(2, 1), xytext=(3, 1.5),
+            arrowprops=dict(facecolor='black', shrink=0.05))
+
+ax.set_ylim(-2, 2)
+# plt.show()
+plt.savefig("1.6.3 Annotations.png")
